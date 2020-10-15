57ec9d15bc03f11ebd7d8d3dc0a2c47e0e97d7e5
Hayden Roper <haydenroper@tamu.edu>
2020-10-15 00:21:12 -0500

    gherkin skeletons added

--- woolala_app/test_driver/features/login_test.feature
+++ woolala_app/test_driver/features/login_test.feature
@@ -1,23 +1,23 @@
 Feature: Validate login screen
 
     Scenario: User launches the app
-        Given The app is open
-        Then I should see "Login With:" my accounts
+        Given The "app" is open
+        Then I should see "Login With:" on my screen
 
     Scenario: Google Login
-        Given My "Google" account is valid
+        Given My "Google" account is "valid"
         When I tap the "Google" button
-        Then I should see the "homepage" screen
+        Then I should see "homepage" on my screen
 
-        Given My "Google" account is not valid
+        Given My "Google" account is "not valid"
         When I tap the "Google" button
-        Then I should see "Login With:" my accounts
+        Then I should see "Login With:" on my screen
 
     Scenario: Facebook Login
-        Given My "Facebook" account is valid
+        Given My "Facebook" account is "valid"
         When I tap the "Facebook" button
-        Then I should see the "homepage" screen
+        Then I should see "homepage" on my screen
 
-        Given My "Facebook" account is not valid
+        Given My "Facebook" account is "not valid"
         When I tap the "Facebook" button
-        Then I should see "Login With:" my accounts
+        Then I should see "Login With:" on my screen
--- woolala_app/test_driver/steps/test_steps.dart
+++ woolala_app/test_driver/steps/test_steps.dart
@@ -2,57 +2,39 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
-StepDefinitionGeneric tapGoogleButton() {
+StepDefinitionGeneric tapLoginButton() {
   return when1<String, FlutterWorld>(
     'I tap the {string} button',
         (key, context) async {
       final locator = find.byValueKey(key);
       await FlutterDriverUtils.tap(context.world.driver, locator);
-     //print("button tapped");
     },
   );
 }
 
-StepDefinitionGeneric tapFacebookButton() {
-  return when1<String, FlutterWorld>(
-    'I tap the {string} button',
+StepDefinitionGeneric appIsOpen() {
+  return given1<String, FlutterWorld>(
+    'The {string} is open',
         (key, context) async {
-      final locator = find.byValueKey(key);
-      await FlutterDriverUtils.tap(context.world.driver, locator);
-      //print("button tapped");
+
     },
   );
 }
 
-StepDefinitionGeneric LoginAPIs() {
+StepDefinitionGeneric iShouldSeeText() {
   return then1<String, FlutterWorld>(
-    'I should see {string} log-in api',
+    'I should see {string} on my screen',
         (key, context) async {
-      // TODO:
-      // key could be either google or facebook
-      // need some code to check it really gets into the login page
+
     },
   );
 }
 
-StepDefinitionGeneric LoggedIn() {
-  return when1<String, FlutterWorld>(
-    'I pass the {string} authentication',
-      (key, context) async {
-      // TODO:
-      // key could be either google or facebook
-      // need some code to check whether the user has successfully logged in
-      },
-  );
-}
+StepDefinitionGeneric accountIsValid() {
+  return given2<String, String, FlutterWorld>(
+    'My {string} account is {string}',
+        (key, validity, context) async {
 
-StepDefinitionGeneric Homepage() {
-  return then<FlutterWorld>(
-    'I should have "homepage" on screen',
-        (context) async {
-      // TODO:
-      // key could be either google or facebook
-      // need some code to check it really gets into the login page
     },
   );
 }
