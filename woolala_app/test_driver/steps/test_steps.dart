8e3893165980db8c8e7265594655699bf49542b9
thomasrost99 <57777616+thomasrost99@users.noreply.github.com>
2020-10-14 22:06:28 -0500

    implemented facebook login backend. Currently reading in profile as json but no UI output

--- woolala_app/test_driver/steps/test_steps.dart
+++ woolala_app/test_driver/steps/test_steps.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
+final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
 StepDefinitionGeneric TapIcons() {
   return when1<String, FlutterWorld>(
@@ -42,6 +49,43 @@ StepDefinitionGeneric Homepage() {
       // TODO:
       // key could be either google or facebook
       // need some code to check it really gets into the login page
+    },
+  );
+}
+
+StepDefinitionGeneric Facebook() {
+  return then<FlutterWorld>(
+    'I should have "homepage" on screen',
+        (context) async {
+          FacebookLogin facebookLogin = FacebookLogin();
+          final result = await facebookLogin.logIn(['email']);
+
+          switch (result.status) {
+            case FacebookLoginStatus.loggedIn:
+              final token = result.accessToken.token;
+
+              final graphResponse = await http.get(
+                  'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
+              final profile = json.decode(graphResponse.body);
+              print(profile);
+              print(profile["name"]);
+              SnackBar googleSnackBar = SnackBar(content: Text("Welcome ${profile["name"]}!"));
+              _scaffoldKey.currentState.showSnackBar(googleSnackBar);
+
+              // final credential = FacebookAuthProvider.getCredential(accessToken: token);
+              // final graphResponse = away http:get()
+              // _showLoggedInUI();
+              break;
+            case FacebookLoginStatus.cancelledByUser:
+            // _showCancelledMessage();
+              print("Sign in failed.");
+              break;
+            case FacebookLoginStatus.error:
+            // _showErrorOnUI(result.errorMessage);
+              print("Sign in failed.");
+
+              break;
+          }
     },
   );
 }
