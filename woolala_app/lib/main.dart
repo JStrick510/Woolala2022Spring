1175bae444f6a4dace8c0ab3cf9f3eb12bf28e76
Bryant Passage <bryant.passage@gmail.com>
2021-04-30 13:04:12 -0500

    added comments on locally defined variables

--- woolala_app/lib/main.dart
+++ woolala_app/lib/main.dart
@@ -36,7 +36,7 @@ class WooLaLa extends StatelessWidget {
       title: 'ChooseNXT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: primaryWhite,
+        primarySwatch: primaryWhite, //locally defined color swatch
         accentColor: Colors.black,
         // This makes the visual density adapt to the platform that you run the app on. For desktop platforms, the controls will be smaller and closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
