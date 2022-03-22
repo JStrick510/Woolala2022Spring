fdcbf72cadf6c83f8847b7d4400764757ebebcd7
JStrick510 <jacobstrickland510@gmail.com>
2022-03-22 07:10:04 -0500

    testing change in domain for heroku deployment

--- woolala_app/lib/main.dart
+++ woolala_app/lib/main.dart
@@ -22,7 +22,7 @@ void main() async {
     domain = "http://0.0.0.0:5000"; //when running Mac
     // domain = "http://Bryants-MacBook-Pro.local:5000";
   } else {
-    domain = "https://hidden-caverns-85596.herokuapp.com";
+    domain = "https://woolala-2022.herokuapp.com";
   }
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
