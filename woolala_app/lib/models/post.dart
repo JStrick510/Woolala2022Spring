079b738ca5ef17e339fc8dd9a3f9cce1d2752460
Farnaz Nazari <62231571+Farnaz12@users.noreply.github.com>
2022-05-09 12:54:00 -0500

    Update post.dart
    adding currency field

--- woolala_app/lib/models/post.dart
+++ woolala_app/lib/models/post.dart
@@ -16,28 +16,30 @@ class Post {
   final String Category;
   final String minprice;
   final String maxprice;
+  final String currency;
 
   Post({
     this.ID, this.UserID, this.ImageID1, this.ImageID2, this.ImageID3, this.ImageID4, this.ImageID5, this.Date,
-    this.Description, this.Comments, this.CumulativeRating, this.NumRatings, this.Category, this.minprice, this.maxprice,
+    this.Description, this.Comments, this.CumulativeRating, this.NumRatings, this.Category, this.minprice, this.maxprice,this.currency,
   });
 
   Post.fromJSON(Map<String, dynamic> json)
-    : ID = json["ID"],
-      UserID = json["UserID"],
-      ImageID1 = json["ImageID1"],
-      ImageID2 = json["ImageID2"],
-      ImageID3 = json["ImageID3"],
-      ImageID4 = json["ImageID4"],
-      ImageID5 = json["ImageID5"],
-      Date = json["Date"],
-      Description = json["Description"],
-      Comments = json["Comments"],
-      CumulativeRating = json["CumulativeRating"],
-      NumRatings = json["NumRatings"],
-      Category = json["Category"],
-      minprice = json["minprice"],
-      maxprice = json["maxprice"];
+      : ID = json["ID"],
+        UserID = json["UserID"],
+        ImageID1 = json["ImageID1"],
+        ImageID2 = json["ImageID2"],
+        ImageID3 = json["ImageID3"],
+        ImageID4 = json["ImageID4"],
+        ImageID5 = json["ImageID5"],
+        Date = json["Date"],
+        Description = json["Description"],
+        Comments = json["Comments"],
+        CumulativeRating = json["CumulativeRating"],
+        NumRatings = json["NumRatings"],
+        Category = json["Category"],
+        minprice = json["minprice"],
+        maxprice = json["maxprice"];
+  currency = json["currency"];
 
   Map<String, dynamic> toJSON() => {
     "ID" : ID,
@@ -55,5 +57,6 @@ class Post {
     "Category" : Category,
     "minprice" : minprice,
     "maxprice" : maxprice,
+    "currency" : currency,
   };
 }
